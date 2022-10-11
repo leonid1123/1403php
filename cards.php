b3badbfbd9034f5b3b45d4819bbdb04559ec8260
Leo <47742726+leonid1123@users.noreply.github.com>
2022-10-11 10:40:58 +0300

    db test

--- cards.php
+++ cards.php
@@ -1,3 +1,15 @@
+<?php
+    $mysqli = new mysqli("localhost", "pk1403", "123456", "cards");
+    echo $mysqli->host_info . "\n";
+    $stmt = $mysqli->prepare("INSERT INTO cardcontent(cardid, bigTitleText, smallTitleText, cardText, buttonText, buttonLink) VALUES (null,?,?,?,?,?)");
+    $bigTitleText = "Большой заголовок ПХП";
+    $smallTitleText = "Маленький заголовок ПХП";
+    $cardText="азазазазазазазаза азазаза зззаааа!";
+    $buttonText = "Пуньк!";
+    $buttonLink = "https://kuda.to";
+    $stmt->bind_param("sssss", $bigTitleText,$smallTitleText,$cardText,$buttonText,$buttonLink);
+    $stmt->execute();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
