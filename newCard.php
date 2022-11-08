c25307a0f93aba35b79afc2bf7793a644db75a4b
Leo <47742726+leonid1123@users.noreply.github.com>
2022-11-08 10:26:00 +0300

    done

--- newCard.php
+++ newCard.php
@@ -1,14 +1,16 @@
 <?php
-/*$mysqli = new mysqli("localhost", "pk1403", "123456", "cards");
-    echo $mysqli->host_info . "\n";
-    $stmt = $mysqli->prepare("INSERT INTO cardcontent(cardid, bigTitleText, smallTitleText, cardText, buttonText, buttonLink) VALUES (null,?,?,?,?,?)");
-    $bigTitleText = "Большой заголовок ПХП";
-    $smallTitleText = "Маленький заголовок ПХП";
-    $cardText="азазазазазазазаза азазаза зззаааа!";
-    $buttonText = "Пуньк!";
-    $buttonLink = "https://kuda.to";
-    $stmt->bind_param("sssss", $bigTitleText,$smallTitleText,$cardText,$buttonText,$buttonLink);
-    $stmt->execute();*/
+if (isset($_POST["go"])) {
+    $mysqli = new mysqli("localhost", "pk1403", "123456", "cards");
+    $stmt = $mysqli->prepare("INSERT INTO cardcontent(cardid, bigTitleText, smallTitleText, cardText, buttonText, buttonLink) 
+VALUES (null,?,?,?,?,?)");
+    $bigTitleText = $_POST["bigTitleText"];
+    $smallTitleText = $_POST["smallTitleText"];
+    $cardText = $_POST["cardText"];
+    $buttonText = $_POST["buttonText"];
+    $buttonLink = $_POST["buttonLink"];
+    $stmt->bind_param("sssss", $bigTitleText, $smallTitleText, $cardText, $buttonText, $buttonLink);
+    $stmt->execute();
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,21 +28,20 @@
     <?php
     include 'pageTop.php';
     ?>
-    <form action="" method="POST">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
         <div class="mb-3">
             <label for="bigTitleText" class="form-label">Укажите большой заголовок</label>
-            <input type="text" class="form-control" id="bigTitleText" placeholder="Заголовок сюда">
+            <input name="bigTitleText" type="text" class="form-control" id="bigTitleText" placeholder="Заголовок сюда">
             <label for="smallTitleText" class="form-label">Укажите малый заголовок</label>
-            <input type="text" class="form-control" id="smallTitleText" placeholder="Заголовок сюда">
+            <input name="smallTitleText" type="text" class="form-control" id="smallTitleText" placeholder="Заголовок сюда">
             <label for="cardText" class="form-label">Введите текст для карточки</label>
-            <textarea class="form-control" id="cardText" rows="3"></textarea>
+            <textarea name="cardText" class="form-control" id="cardText" rows="3"></textarea>
             <label for="buttonText" class="form-label">Текст на кнопке</label>
-            <input type="text" class="form-control" id="buttonText" placeholder="Текст кнопки сюда">
+            <input name="buttonText" type="text" class="form-control" id="buttonText" placeholder="Текст кнопки сюда">
             <label for="buttonLink" class="form-label">Укажите текст ссылки, начиная с https://</label>
-            <input type="url" class="form-control" id="buttonLink" placeholder="https://">
-            <button type="submit" class="btn btn-danger mb-3 ">Отправить</button>
+            <input name="buttonLink" type="url" class="form-control" id="buttonLink" placeholder="https://">
+            <button name="go" type="submit" class="btn btn-danger mb-3 ">Отправить</button>
         </div>
-
     </form>
     </div>
 </body>
